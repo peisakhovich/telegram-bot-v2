54d46ae8a0752ffa9aeba327a80d00f6a2815bd4
Grigorii Peisakhovich <154021703+peisakhovich@users.noreply.github.com>
2026-06-03 18:26:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from datetime import datetime
+import db
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    await db.init_db()
+
 @app.get("/health")
-async def health():
+def health():
     return {"status": "ok"}
+
+@app.get("/test-insert")
+async def test_insert():
+    await db.insert_message({
+        "channel_name": "pga_news_monitor",
+        "message_text": "test message from azure",
+        "username": "system",
+        "user_id": "0",
+        "timestamp": datetime.utcnow()
+    })
+    return {"insert": "ok"}
